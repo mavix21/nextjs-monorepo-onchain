9fc028a0a375bf72f1cf18801ca2117f737d2106
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-25 13:40:11 -0500

    feat: add auth shared package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import type { BetterAuthOptions, BetterAuthPlugin } from "better-auth";
+import { betterAuth } from "better-auth";
+
+export function initAuth<TExtraPlugins extends BetterAuthPlugin[]>(options: {
+  baseUrl: string;
+  productionUrl: string;
+  secret: string | undefined;
+
+  extraPlugins?: TExtraPlugins;
+}) {
+  const config = {
+    baseURL: options.baseUrl,
+    secret: options.secret,
+    onAPIError: {
+      onError(error, ctx) {
+        console.error("BETTER AUTH API ERROR:", { error, ctx });
+      },
+    },
+  } satisfies BetterAuthOptions;
+
+  return betterAuth(config);
+}
+
+export type Auth = ReturnType<typeof initAuth>;
+export type Session = Auth["$Infer"]["Session"];
