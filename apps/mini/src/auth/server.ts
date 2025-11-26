26390794497064d21acf776743885ba4c0281d79
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-25 22:51:36 -0500

    feat: implement getToken and api logic using better-auth convex integration

--- apps/mini/src/auth/server.ts
+++ apps/mini/src/auth/server.ts
@@ -1,27 +1,9 @@
 import "server-only";
 
-import { cache } from "react";
-import { headers } from "next/headers";
-import { siwf } from "better-auth-siwf";
+import { getToken as getTokenNextjs } from "@convex-dev/better-auth/nextjs";
 
-import { initAuth } from "@myapp/auth";
+import { createAuth } from "@myapp/convex/auth";
 
-import { env } from "@/env";
-import { ROOT_URL } from "@/lib/constants";
-
-const baseUrl = ROOT_URL;
-
-export const auth = initAuth({
-  baseUrl,
-  productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`,
-  secret: env.AUTH_SECRET,
-  extraPlugins: [
-    siwf({
-      hostname: new URL(baseUrl).hostname,
-    }),
-  ],
-});
-
-export const getSession = cache(async () =>
-  auth.api.getSession({ headers: await headers() }),
-);
+export const getToken = () => {
+  return getTokenNextjs(createAuth);
+};
