e41fb4a493ccc2de4e514d9f24c0654c77a68313
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-26 21:58:17 -0500

    refactor: move pages inside (app) folder

--- apps/mini/app/(app)/page.tsx
+++ apps/mini/app/(app)/page.tsx
@@ -3,9 +3,15 @@
 import Image from "next/image";
 import { Wallet } from "@coinbase/onchainkit/wallet";
 
+import { Button } from "@myapp/ui/components/button";
+
+import { useAuth } from "@/app/_contexts/auth-context";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  const { signIn, error } = useAuth();
+
   return (
     <div className={styles.container}>
       <header className={styles.headerWrapper}>
@@ -23,8 +29,10 @@ export default function Home() {
         <h1 className={`${styles.title} font-accent`}>MiniKit</h1>
 
         <p>
-          Get started by editing <code>app/page.tsx</code>
+          Start by editing <code>app/page.tsx</code>
         </p>
+        <Button onClick={signIn}>Sign In</Button>
+        {error && <p className={styles.error}>Error: {error.message}</p>}
 
         <h2 className={styles.componentsTitle}>Explore Components</h2>
 
