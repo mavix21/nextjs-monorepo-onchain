c5851dc0cc5b74b0fcb0ed85ae930a10c9f8bf49
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-29 17:34:57 -0500

    feat: add SITE_URL and NEXT_PUBLIC_CONVEX_URL to environment variables schema

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -15,6 +15,7 @@ export const env = createEnv({
    */
   server: {
     // POSTGRES_URL: z.string().url(),
+    SITE_URL: z.url(),
   },
 
   /**
@@ -23,12 +24,14 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CONVEX_URL: z.url(),
   },
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
   skipValidation:
     !!process.env.CI || process.env.npm_lifecycle_event === "lint",
