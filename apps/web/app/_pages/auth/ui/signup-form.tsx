21718815fa69a0d504c75f78d99b1b16b5f15e86
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-29 22:03:28 -0500

    feat: update SignupForm to use SignInWithBase component and improve layout

--- apps/web/app/_pages/auth/ui/signup-form.tsx
+++ apps/web/app/_pages/auth/ui/signup-form.tsx
@@ -1,4 +1,3 @@
-import { SignInWithBaseButton } from "@base-org/account-ui/react";
 import { GalleryVerticalEnd } from "lucide-react";
 
 import { Button } from "@myapp/ui/components/button";
@@ -9,6 +8,8 @@ import {
   FieldSeparator,
 } from "@myapp/ui/components/field";
 import { cn } from "@myapp/ui/lib/utils";
+
+import { SignInWithBase } from "@/widgets/auth";
 
 export function SignupForm({
   className,
@@ -28,38 +29,21 @@ export function SignupForm({
               </div>
               <span className="sr-only">Acme Inc.</span>
             </a>
-            <h1 className="text-xl font-bold">Welcome to Acme Inc.</h1>
-            {/* <FieldDescription>
-              Already have an account? <a href="#">Sign in</a>
-            </FieldDescription> */}
+            <h1 className="text-xl font-bold">Welcome to Builders</h1>
           </div>
-          <Field>
-            {/* <FieldLabel htmlFor="email">Email</FieldLabel>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            /> */}
-            <SignInWithBaseButton
-              align="center"
-              variant="solid"
-              colorScheme="light"
-            />
+          <Field className="h-14">
+            <SignInWithBase />
           </Field>
-          <Field>
-            <Button type="submit">Create Account</Button>
-          </Field>
-          <FieldSeparator>Or</FieldSeparator>
+          <FieldSeparator>Or continue with</FieldSeparator>
           <Field className="grid gap-4 sm:grid-cols-2">
             <Button variant="outline" type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path
-                  d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                  d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
                   fill="currentColor"
-                />
+                ></path>
               </svg>
-              Continue with Apple
+              GitHub
             </Button>
             <Button variant="outline" type="button">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -68,7 +52,7 @@ export function SignupForm({
                   fill="currentColor"
                 />
               </svg>
-              Continue with Google
+              Google
             </Button>
           </Field>
         </FieldGroup>
