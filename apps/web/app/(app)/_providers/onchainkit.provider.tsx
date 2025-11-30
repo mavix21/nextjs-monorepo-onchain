47f9f33e35cd41b713cbd412145a72a94bfa6a23
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-29 23:59:42 -0500

    feat: add OnchainKitClientProvider component for on-chain interactions

---
+++ apps/web/app/(app)/_providers/onchainkit.provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import { base } from "wagmi/chains";
+
+import "@coinbase/onchainkit/styles.css";
+
+import { env } from "@/env";
+
+export function OnchainKitClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <OnchainKitProvider
+      apiKey={env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base}
+      config={{
+        appearance: {
+          mode: "auto",
+        },
+        wallet: {
+          display: "modal",
+        },
+      }}
+    >
+      {children}
+    </OnchainKitProvider>
+  );
+}
