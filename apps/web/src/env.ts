6b7f00e3bd80f789094224a294aea7a1cd589a14
mavix21 <marcevizcarra2109@hotmail.com>
2025-11-29 23:59:56 -0500

    feat: add NEXT_PUBLIC_ONCHAINKIT_API_KEY to client and experimental runtime environment

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -26,6 +26,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_ONCHAINKIT_API_KEY: z.string(),
     NEXT_PUBLIC_CONVEX_URL: z.url(),
   },
   /**
@@ -33,6 +34,7 @@ export const env = createEnv({
    */
   experimental__runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_ONCHAINKIT_API_KEY: process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
   skipValidation:
