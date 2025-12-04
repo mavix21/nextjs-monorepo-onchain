8dbca98290297902c755e12242e3737dfba63e59
mavix21 <marcevizcarra2109@hotmail.com>
2025-12-04 00:05:25 -0500

    feat: add SheetLink component to append current pathname as query parameter

---
+++ apps/mini/app/_shared/ui/sheet-link.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import type { Route } from "next";
+import type { ComponentProps } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type SheetLinkProps = Omit<ComponentProps<typeof Link>, "href"> & {
+  href: Route;
+};
+
+/**
+ * A Link that automatically appends the current pathname as `?origin=`
+ * so sheet routes can return to where the user came from.
+ */
+export function SheetLink({ href, ...props }: SheetLinkProps) {
+  const pathname = usePathname();
+  const hrefWithOrigin =
+    `${href}?origin=${encodeURIComponent(pathname)}` as Route;
+
+  return <Link href={hrefWithOrigin} {...props} />;
+}
